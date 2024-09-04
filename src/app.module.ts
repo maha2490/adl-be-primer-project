bb49a10b13907b7cee7d5b1fda55c0489cb65de5
Rachel Mahan <rachelm2490@gmail.com>
2024-09-04 13:19:55 -0400

    add graphql + todo

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TodoModule } from './todo/todo.module';
 
 @Module({
-  imports: [],
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      playground: true,
+      typePaths: ['./**/*.graphql'],
+    }),
+    TodoModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
